20258e149ae1bca010d4c2488b7081bcd0d4088f
Mike Graham <mgraham@technicalmedia.com>
2015-06-27 19:48:09 -0600

    created deployment script to copy new bundles and new .d.ts files to various samples for testing.  so far, just skel-nav-esri-vs-ts

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+# deploy to skel-nav-esri-vs-ts
+cp scripts/aurelia/* ../aurelia-typescript/skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/scripts/aurelia
+cp typings/aurelia/* ../aurelia-typescript/skel-nav-esri-vs-ts/skel-nav-esri-vs-ts/typings/aurelia
