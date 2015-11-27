ae7982ace7a8698b0f4d2b033793fd58cb22d5db
Mike Graham <mgraham@technicalmedia.com>
2015-11-27 02:33:14 -0700

    removed core-js from bundle in favor of external reference.  dojo couldn't seem to use core-js from the bundle.  both requirejs and dojo work fine with core-js as external reference.

--- bundle-and-copy.sh
+++ bundle-and-copy.sh
@@ -1,8 +1,8 @@
 echo build aurelia-bundle
-node r.js -o name=aurelia-bundle-manifest baseUrl=. mainConfigFile=main-config.js out=aurelia-bundle.js optimize=none
+node r.js -o name=aurelia-bundle-manifest baseUrl=. mainConfigFile=main-config.js exclude=core-js out=aurelia-bundle.js optimize=none
 
 echo build aurelia-bundle-latest
-node r.js -o name=aurelia-bundle-manifest baseUrl=. mainConfigFile=main-config-latest.js out=aurelia-bundle-latest.js optimize=none
+node r.js -o name=aurelia-bundle-manifest baseUrl=. mainConfigFile=main-config-latest.js exclude=core-js out=aurelia-bundle-latest.js optimize=none
 
 cp -f aurelia-bundle.js aurelia-bundle-latest.js scripts/aurelia
 cp -f aurelia-latest/*/dist/amd/*.d.ts typings/aurelia
